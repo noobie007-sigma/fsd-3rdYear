5350c59e868348ad78b747aeb9cee7f80143f645
noobie007-sigma <sukjais7120@gmail.com>
2024-10-21 11:54:00 +0530

    Kem Cho?

--- Day6/script.js
+++ Day6/script.js
@@ -1,41 +1,53 @@
-// // let a = () => {
-// //     console.log("Arrow Function")
+// // // let a = () => {
+// // //     console.log("Arrow Function")
+// // // }
+
+// // // a();
+// // // let b = () => console.log("Single line statement execute krne ke liye ye format use karo")
+// // // b();
+
+// // let c = (x, y) => x+y;
+// // console.log(c("Rahul", "Ravi"))
+// // console.log(c(12,21))
+// // function display() {
+// //     let p1 = document.querySelectorAll('p')
+// //     p1[0].innerHTML = "I am paragraph"
+// //     p1[0].style.backgroundColor = 'red'
+// //     p1[1].innerHTML = "I am 2nd paragraph"
+// //     p1[1].style.backgroundColor = 'green'
+// // }
+// // function changeText() {
+// //     let heading = document.querySelector('h1')
+// //     heading.textContent = "Changed Heading"
+// //     let a1 = document.querySelector('a')
+// //     a1.setAttribute('href', 'https://aktu.ac.in')
+// //     a1.setAttribute('target', '_blank')
+// //     a1.innerHTML = "AKTU Website"
+// // }
+// // let createNewDiv = () => {
+// //     const newDiv = document.createElement('div')
+// //     // newDiv.style.backgroundColor = 'red'
+// //     // newDiv.style.height = '20px'
+// //     // newDiv.style.width = '20px'
+// //     newDiv.textContent = "This is newly added content"
+
+// //     const bod = document.querySelector('body')
+// //     bod.appendChild('newDiv')
 // // }
 
-// // a();
-// // let b = () => console.log("Single line statement execute krne ke liye ye format use karo")
-// // b();
+// const but = document.querySelector('button')
+// but.addEventListener('click', () => {
+//     document.getElementById('res').innerHTML = "Updated"
+// });
 
-// let c = (x, y) => x+y;
-// console.log(c("Rahul", "Ravi"))
-// console.log(c(12,21))
-// function display() {
-//     let p1 = document.querySelectorAll('p')
-//     p1[0].innerHTML = "I am paragraph"
-//     p1[0].style.backgroundColor = 'red'
-//     p1[1].innerHTML = "I am 2nd paragraph"
-//     p1[1].style.backgroundColor = 'green'
-// }
-// function changeText() {
-//     let heading = document.querySelector('h1')
-//     heading.textContent = "Changed Heading"
-//     let a1 = document.querySelector('a')
-//     a1.setAttribute('href', 'https://aktu.ac.in')
-//     a1.setAttribute('target', '_blank')
-//     a1.innerHTML = "AKTU Website"
-// }
-// let createNewDiv = () => {
-//     const newDiv = document.createElement('div')
-//     // newDiv.style.backgroundColor = 'red'
-//     // newDiv.style.height = '20px'
-//     // newDiv.style.width = '20px'
-//     newDiv.textContent = "This is newly added content"
-
-//     const bod = document.querySelector('body')
-//     bod.appendChild('newDiv')
-// }
-
-const but = document.querySelector('button')
-but.addEventListener('click', () => {
-    document.getElementById('res').innerHTML = "Updated"
-});
+console.log("start");
+setTimeout(() => {
+    console.log("First task completed")
+    setTimeout(() => {
+        console.log("Second task completed")
+        setTimeout(() => {
+            console.log("Thied task completed")
+        }, 3000)
+    }, 2000)
+}, 1000)
+console.log("End")
