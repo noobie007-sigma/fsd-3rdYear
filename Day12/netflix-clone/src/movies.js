43419439599a3dfa949bc2333c70a193d98875a7
noobie007-sigma <sukjais7120@gmail.com>
2024-11-18 12:56:23 +0530

    Netflix Clone

---
+++ Day12/netflix-clone/src/movies.js
@@ -0,0 +1,75 @@
+const movies = [
+    {
+        id: 1,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+        title: "TARZAN-THE WONDER CAR",
+        language: "Hindi",
+        cost: 250
+    },
+    {
+        id: 2,
+        image: "https://rukminim2.flixcart.com/image/612/612/jc6jl3k0/movie/c/w/z/2010-blu-ray-eagle-english-ong-bak-3-blu-ray-original-imaffd7uz3ax9bpu.jpeg?q=70",
+        title: "Ong Bak 3 (Blu Ray)",
+        language: "English",
+        cost: 350
+    },
+    {
+        id: 3,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/e/i/f/2012-dvd-soni-hindi-the-adventures-of-tintin-the-secret-of-the-original-imagqbtvtakrzg8f.jpeg?q=70",
+        title: "THE ADVENTURES OF TINTIN - THE SECRET OF THE UNICORN",
+        language: "Hindi",
+        cost: 250
+    },
+    {
+        id: 4,
+        image: "https://rukminim2.flixcart.com/image/612/612/k3dc7m80/movie/t/c/f/2018-blu-ray-universal-pictures-excel-home-videos-english-original-imafmggqgub6vreg.jpeg?q=70",
+        title: "Jurassic World: Fallen Kingdom",
+        language: "English",
+        cost: 350
+    },
+    {
+        id: 5,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/s/t/2023-blu-ray-paramount-pictures-english-mission-impossible-dead-original-imaguw429xnwybtg.jpeg?q=70",
+        title: "Mission: Impossible – Dead Reckoning Part",
+        language: "English",
+        cost: 550
+    },
+    {
+        id: 6,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/s/t/2023-blu-ray-paramount-pictures-english-mission-impossible-dead-original-imaguw429xnwybtg.jpeg?q=70",
+        title: "TARZAN-THE WONDER CAR",
+        language: "Hindi",
+        cost: 250
+    },
+    {
+        id: 7,
+        image: "https://rukminim2.flixcart.com/image/612/612/jc6jl3k0/movie/c/w/z/2010-blu-ray-eagle-english-ong-bak-3-blu-ray-original-imaffd7uz3ax9bpu.jpeg?q=70",
+        title: "Ong Bak 3 (Blu Ray)",
+        language: "English",
+        cost: 350
+    },
+    {
+        id: 8,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/e/i/f/2012-dvd-soni-hindi-the-adventures-of-tintin-the-secret-of-the-original-imagqbtvtakrzg8f.jpeg?q=70",
+        title: "THE ADVENTURES OF TINTIN - THE SECRET OF THE UNICORN",
+        language: "Hindi",
+        cost: 250
+    },
+    {
+        id: 9,
+        image: "https://rukminim2.flixcart.com/image/612/612/k3dc7m80/movie/t/c/f/2018-blu-ray-universal-pictures-excel-home-videos-english-original-imafmggqgub6vreg.jpeg?q=70",
+        title: "Jurassic World: Fallen Kingdom",
+        language: "English",
+        cost: 350
+    },
+    {
+        id: 10,
+        image: "https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70",
+        title: "Gangs Of Wasseypur I",
+        language: "Hindi",
+        cost: 150
+    }
+
+]
+
+export default movies;
